690bf6735fd88f4389608c55bc632a53c74f023f
Алексей Леонтьев <leontev.leha06@bk.ru>
2024-12-24 13:50:15 +0500

    Добавление пакетов. Генерация контекста данных для существующей БД

---
+++ LB3_blog/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LB3_blog;
+
+public partial class Post
+{
+    public short Id { get; set; }
+
+    public short IdUser { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Content { get; set; } = null!;
+
+    public DateTime DateOfPublication { get; set; }
+
+    public virtual User IdUserNavigation { get; set; } = null!;
+}
